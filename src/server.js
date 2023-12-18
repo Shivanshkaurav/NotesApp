4266e0777b903dbc706a3503b4cdde3b519b95bb
Shivansh <shivanshkaurav04@gmail.com>
2023-12-19 00:06:27 +0530

    Notes Server

---
+++ src/server.js
@@ -0,0 +1,26 @@
+// Initialization
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+const mongoDbPath = "mongodb+srv://Shivansh04:Shivansh04@cluster0.fburzrl.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoDbPath).then(function(){
+
+    app.get("/", function(req, res){
+        const response = {message: "API Works!"};
+        res.json(response);
+    });
+    
+    const noteRouter = require('./Route/Note');
+    app.use("/notes", noteRouter);
+});
+
+// Starting Server on a Port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, function(){
+    console.log("App Running on the PORT:" + PORT);
+})
